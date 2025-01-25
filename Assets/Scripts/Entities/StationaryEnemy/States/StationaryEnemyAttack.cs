ef8f805b1490553cf9bfb17d91c58364f1d569be
Okan Ozbek <devokan@outlook.com>
2025-01-25 16:57:40 +0100

    Made enemy look at player during attack

--- Assets/Scripts/Entities/StationaryEnemy/States/StationaryEnemyAttack.cs
+++ Assets/Scripts/Entities/StationaryEnemy/States/StationaryEnemyAttack.cs
@@ -1,4 +1,4 @@
-﻿using UnityEngine;
+using UnityEngine;
 using Utility;
 
 namespace Entities.StationaryEnemy.States
@@ -19,11 +19,28 @@ namespace Entities.StationaryEnemy.States
         public override void Update()
         {
             _hasDetectedPlayer = DetectCollider(UnityTag.Player);
+            CollidersInAggroRange(UnityTag.Player);
+            
+            RotateTowardsTarget();
         }
 
         protected override void SetTransitions()
         {
             AddTransition(StationaryEnemyStateType.Idle, () => _hasDetectedPlayer == false);
+        }
+        
+        private void RotateTowardsTarget()
+        {
+            if (AggroTargetCollider == false)
+            {
+                return;
+            }
+            
+            Vector2 direction = (AggroTargetCollider.transform.position - Controller.transform.position).normalized;
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+            Quaternion target = Quaternion.Euler(0f, 0f, angle);
+            
+            Controller.transform.rotation = Quaternion.RotateTowards(Controller.transform.rotation, target, Controller.stats.rotationSpeed * Time.deltaTime);
         }
     }
 }
--- Assets/Scripts/Entities/StationaryEnemy/States/StationaryEnemyState.cs
+++ Assets/Scripts/Entities/StationaryEnemy/States/StationaryEnemyState.cs
@@ -1,4 +1,5 @@
-﻿using FSM;
+﻿using System.Collections.Generic;
+using FSM;
 using UnityEngine;
 using Utility;
 
@@ -8,9 +9,54 @@ namespace Entities.StationaryEnemy.States
     {
         protected readonly StationaryEnemyController Controller;
         
+        protected Collider2D AggroTargetCollider;
+        protected Collider2D AttackTargetCollider;
+        
+        private readonly List<Collider2D> _interactedColliders = new();
+        
         protected StationaryEnemyState(StationaryEnemyController controller)
         {
             Controller = controller;
+        }
+        
+        protected bool CollidersInAggroRange(UnityTag objectWithTag)
+        {
+            Collider2D[] colliders = Physics2D.OverlapCircleAll(
+                Controller.transform.position, 
+                Controller.stats.detectionRadius
+                );
+            
+            foreach (Collider2D collider in colliders)
+            {
+                if (collider.CompareTag(objectWithTag.ToString()))
+                {
+                    AggroTargetCollider = collider;
+                    return true;
+                }
+            }
+
+            AggroTargetCollider = null;
+            return false;
+        }
+        
+        protected bool CollidersInAttackRange(UnityTag objectWithTag)
+        {
+            Collider2D[] colliders = Physics2D.OverlapCircleAll(
+                Controller.transform.position, 
+                Controller.stats.attackRadius
+                );
+            
+            foreach (Collider2D collider in colliders)
+            {
+                if (collider.CompareTag(objectWithTag.ToString()))
+                {
+                    AttackTargetCollider = collider;
+                    return true;
+                }
+            }
+
+            AttackTargetCollider = null;
+            return false;
         }
 
         protected bool DetectCollider(UnityTag objectWithTag)
