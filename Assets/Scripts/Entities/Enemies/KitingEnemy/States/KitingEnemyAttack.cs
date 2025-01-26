98c32fb39c1a990bf6fa1b1d51dac82ba33da36e
Okan Ozbek <devokan@outlook.com>
2025-01-26 05:35:36 +0100

    Shoot towards predicted player area

--- Assets/Scripts/Entities/Enemies/KitingEnemy/States/KitingEnemyAttack.cs
+++ Assets/Scripts/Entities/Enemies/KitingEnemy/States/KitingEnemyAttack.cs
@@ -7,27 +7,22 @@ namespace Entities.Enemies.KitingEnemy.States
     public class KitingEnemyAttack : KitingEnemyState
     {
         private bool _inAggroRange;
-        
+        private Transform _playerTransform;
+        private Rigidbody2D _playerRigidbody;
+
         public KitingEnemyAttack(KitingEnemyController controller) : base(controller)
         {
         }
-        
+
         public override void Enter()
         {
-            Controller.spriteRenderer.color = Color.magenta;
-            
-            GameObject instance = Object.Instantiate(
-                original: Controller.weapon.prefab,
-                position: Controller.weapon.pivotPoint.position,
-                rotation: Controller.transform.rotation
-                );
-            
-            instance.GetComponent<KitingEnemyProjectile>().Setup(
-                Controller.weapon.damage, 
-                Controller.weapon.enemyKnockbackForce,
-                Controller.weapon.shakeIntensity
-                );
-            
+            CollidersInAggroRange(UnityTag.Player);
+            _playerTransform = AggroTargetCollider.gameObject.transform;
+            _playerRigidbody = _playerTransform.GetComponentInParent<Rigidbody2D>();
+
+            Vector2 predictedPosition = PredictPlayerPosition();
+            ShootProjectile(predictedPosition);
+
             Controller.weapon.onCooldown = true;
         }
 
@@ -35,7 +30,7 @@ namespace Entities.Enemies.KitingEnemy.States
         {
             _inAggroRange = CollidersInAggroRange(UnityTag.Player);
             CollidersInAggroRange(UnityTag.Player);
-            
+
             RotateTowardsTarget();
         }
 
@@ -44,19 +39,48 @@ namespace Entities.Enemies.KitingEnemy.States
             AddTransition(KitingEnemyStateType.AttackWait, () => Controller.weapon.onCooldown);
             AddTransition(KitingEnemyStateType.Idle, () => _inAggroRange == false);
         }
-        
+
         private void RotateTowardsTarget()
         {
             if (AggroTargetCollider == false)
             {
                 return;
             }
-            
-            Vector2 direction = (AggroTargetCollider.transform.position - Controller.transform.position).normalized;
+
+            Vector2 direction = ((Vector3)PredictPlayerPosition() - Controller.transform.position).normalized;
             float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
             Quaternion target = Quaternion.Euler(0f, 0f, angle);
-            
+
             Controller.transform.rotation = Quaternion.RotateTowards(Controller.transform.rotation, target, Controller.stats.rotationSpeed * Time.deltaTime);
+        }
+
+        private Vector2 PredictPlayerPosition()
+        {
+            float projectileSpeed = Controller.weapon.prefab.GetComponent<KitingEnemyProjectile>().speed;
+            Vector2 playerPosition = _playerTransform.position;
+            Vector2 playerVelocity = _playerRigidbody.linearVelocity;
+            float projectileTravelTime = Vector2.Distance(Controller.transform.position, playerPosition) / projectileSpeed;
+
+            return playerPosition + playerVelocity * projectileTravelTime;
+        }
+
+        private void ShootProjectile(Vector2 targetPosition)
+        {
+            Vector2 direction = (targetPosition - (Vector2)Controller.weapon.pivotPoint.position).normalized;
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+            Quaternion rotation = Quaternion.Euler(0f, 0f, angle);
+
+            GameObject instance = Object.Instantiate(
+                original: Controller.weapon.prefab,
+                position: Controller.weapon.pivotPoint.position,
+                rotation: rotation
+            );
+
+            instance.GetComponent<KitingEnemyProjectile>().Setup(
+                Controller.weapon.damage,
+                Controller.weapon.enemyKnockbackForce,
+                Controller.weapon.shakeIntensity
+            );
         }
     }
 }
